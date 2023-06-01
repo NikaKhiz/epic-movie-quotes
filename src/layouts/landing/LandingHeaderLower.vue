76d17901a1e3c0242a69cba8e36a7478ac05d433
NikaKhiz <nika.khizambareli@redberry.ge>
2023-06-02 00:31:17 +0400

    feat(landing): make header fixed

--- src/layouts/landing/LandingHeaderLower.vue
+++ src/layouts/landing/LandingHeaderLower.vue
@@ -2,7 +2,7 @@
 import ButtonPrimary from "@/components/ui/buttons/ButtonPrimary.vue";
 </script>
 <template>
-  <div class="py-28 md:py-64 flex flex-col gap-8 md:gap-6 items-center">
+  <div class="py-44 md:py-80 flex flex-col gap-8 md:gap-6 items-center">
     <h1
       class="text-brownyWhite font-bold text-2xl md:text-6xl md:leading-[150%] max-w-[280px] md:max-w-[700px] text-center"
     >
