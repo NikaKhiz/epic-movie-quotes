6199531d53215d5003a1965260f001b0cea07576
NikaKhiz <nika.khizambareli@redberry.ge>
2023-06-01 14:32:32 +0400

    initial setup

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <div class="min-w-screen min-h-screen grid place-items-center">
+    <h2 class="font-bold text-5xl text-red-500">Upgraded Movie Quotes</h2>
+  </div>
+</template>
+
+<style scoped></style>
