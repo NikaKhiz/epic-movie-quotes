9ccaec808e857adfb5af88ead725a93e688c47c5
NikaKhiz <nika.khizambareli@redberry.ge>
2023-06-07 18:16:53 +0400

    fix(buttons): correct styles

--- src/components/ui/buttons/ButtonPrimary.vue
+++ src/components/ui/buttons/ButtonPrimary.vue
@@ -1,16 +1,26 @@
 <script setup>
+import { computed } from "vue";
+
 const props = defineProps({
   size: {
     type: String,
+    default: "large",
   },
+});
+
+const smallButton = computed(() => {
+  return props.size === "small";
+});
+const largeButton = computed(() => {
+  return props.size === "large";
 });
 </script>
 <template>
   <button
-    class="bg-neutralRed text-neutralWhite rounded-md transition-colors hover:bg-shinyRed"
+    class="bg-neutralRed border-solid border-1 border-neutralRed text-neutralWhite rounded-md transition-colors hover:bg-shinyRed"
     :class="{
-      'px-4 md:px-6 py-1 md:py-2 text-sm md:text-base': props.size === 'small',
-      'px-3 md:px-4 py-2 md:py-2 text-base md:text-xl': props.size === 'large',
+      'px-4 md:px-6 py-1 md:py-2 text-sm md:text-base': smallButton,
+      'px-3 md:px-4 py-2 md:py-2 text-base md:text-xl': largeButton,
     }"
   >
     <slot></slot>
--- src/components/ui/buttons/ButtonSecondary.vue
+++ src/components/ui/buttons/ButtonSecondary.vue
@@ -1,6 +1,33 @@
+<script setup>
+import { computed } from "vue";
+
+const props = defineProps({
+  size: {
+    type: String,
+    required: false,
+    default: "large",
+  },
+  type: {
+    type: String,
+    required: false,
+  },
+});
+
+const smallButton = computed(() => {
+  return props.size === "small";
+});
+const largeButton = computed(() => {
+  return props.size === "large";
+});
+</script>
 <template>
   <button
-    class="text-neutralWhite border border-neutralWhite gap-3 px-4 md:px-6 py-1 md:py-2 text-sm md:text-base rounded-md"
+    class="flex items-center justify-center text-neutralWhite border-solid border-1 border-neutralWhite gap-2 px-4 md:px-6 text-sm md:text-base rounded-md"
+    :class="{
+      'py-1 md:py-2 ': smallButton,
+      'py-2 md:py-2 ': largeButton,
+    }"
+    :type="type"
   >
     <slot></slot>
   </button>
