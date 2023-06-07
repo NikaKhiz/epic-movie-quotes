6276a6b5f569a4655d6e7c564666c4c7ac279801
NikaKhiz <nika.khizambareli@redberry.ge>
2023-06-07 17:49:16 +0400

    feat: define modal store

---
+++ src/stores/modalStore.js
@@ -0,0 +1,19 @@
+import { defineStore } from "pinia";
+
+export const useCounterStore = defineStore("modalStore", {
+  state: () => {
+    return {
+      dialogLogIn: false,
+      dialogSignUp: false,
+      dialogGetStarted: false,
+      dialogEmailVerification: false,
+      dialogEmailResetNotification: false,
+      dialogAccauntActivated: false,
+      dialogPasswordRecovery: false,
+      dialogPasswordRecoveryNotification: false,
+      dialogPasswordReset: false,
+      dialogPasswordResetNotification: false,
+      dialogLinkExpiration: false,
+    };
+  },
+});
