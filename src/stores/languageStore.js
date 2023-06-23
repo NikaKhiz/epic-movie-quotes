80072fd0c8a17ddd5ebf84cf3a09759439cf163f
NikaKhiz <nika.khizambareli@redberry.ge>
2023-06-23 15:10:38 +0400

    refactor(store): language

--- src/stores/languageStore.js
+++ src/stores/languageStore.js
@@ -1,14 +1,12 @@
 import { defineStore } from "pinia";
 import { getCurrentLocale, getFallbackLocale } from "@/utils/getLocale";
-export const useLanguageStore = defineStore("languageStore", {
-  state: () => {
-    return {
-      locale: getCurrentLocale(),
-      fallbackLocale: getFallbackLocale(),
-    };
-  },
-  getters: {
-    currentLocale: (state) => state.locale,
-    fallBack: (state) => state.fallbackLocale,
-  },
+import { computed, ref } from "vue";
+export const useLanguageStore = defineStore("languageStore", () => {
+  const locale = ref(getCurrentLocale());
+  const fallbackLocale = ref(getFallbackLocale());
+
+  const currentLocale = computed(() => locale.value);
+  const fallBack = computed(() => fallbackLocale.value);
+
+  return { locale, fallbackLocale, currentLocale, fallBack };
 });
