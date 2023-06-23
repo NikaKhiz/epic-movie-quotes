5e896c9104b3707332803f579d54fece2555ee75
NikaKhiz <nika.khizambareli@redberry.ge>
2023-06-23 15:02:38 +0400

    refactor: modalstore

--- src/stores/modalStore.js
+++ src/stores/modalStore.js
@@ -1,21 +1,21 @@
 import { defineStore } from "pinia";
+import { ref } from "vue";
 
-export const useModalStore = defineStore("modalStore", {
-  state: () => {
-    return {
-      dialog: {
-        logIn: false,
-        signUp: false,
-        emailVerification: false,
-        emailResetNotification: false,
-        accauntActivated: false,
-        passwordRecovery: false,
-        passwordRecoveryNotification: false,
-        passwordReset: false,
-        passwordResetNotification: false,
-        linkExpiration: false,
-      },
-      activeDialog: "",
-    };
-  },
+export const useModalStore = defineStore("modalStore", () => {
+  const dialog = ref({
+    logIn: false,
+    signUp: false,
+    emailVerification: false,
+    emailResetNotification: false,
+    accauntActivated: false,
+    passwordRecovery: false,
+    passwordRecoveryNotification: false,
+    passwordReset: false,
+    passwordResetNotification: false,
+    linkExpiration: false,
+  });
+
+  const activeDialog = ref("");
+
+  return { dialog, activeDialog };
 });
