708ab03f668731fe736b06309cddb439cc3c0e0f
NikaKhiz <nika.khizambareli@redberry.ge>
2023-06-23 15:07:55 +0400

    refactor(store): backendvalidations

--- src/stores/backEndValidationStore.js
+++ src/stores/backEndValidationStore.js
@@ -1,9 +1,11 @@
 import { defineStore } from "pinia";
+import { ref } from "vue";
 
-export const useBackErrorsStore = defineStore("backErrorsStore", {
-  state: () => {
-    return {
-      errors: {},
-    };
-  },
+export const useBackErrorsStore = defineStore("backErrorsStore", () => {
+  const errors = ref({});
+  function $reset() {
+    errors.value = {};
+  }
+
+  return { errors, $reset };
 });
