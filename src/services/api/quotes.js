a39524eea11df8b75e2084a0c4e6aaba9e86f097
NikaKhiz <nika.khizambareli@redberry.ge>
2023-07-12 14:33:56 +0400

    feat: quote crud services

---
+++ src/services/api/quotes.js
@@ -0,0 +1,32 @@
+import axios from "@/plugins/axios/axiosMultipart.js";
+
+export const addQuote = async (title, titleKa, thumbnail, movieId) => {
+  const response = await axios.post("/api/quotes", {
+    title,
+    title_ka: titleKa,
+    thumbnail,
+    movie_id: movieId,
+  });
+  return response;
+};
+export const updateQuote = async (title, titleKa, thumbnail, quoteId) => {
+  const response = await axios.post(`/api/quotes/${quoteId}`, {
+    title,
+    title_ka: titleKa,
+    thumbnail,
+  });
+  return response;
+};
+export const destroyQuote = async (quoteId) => {
+  const response = await axios.delete(`/api/quotes/${quoteId}`);
+  return response;
+};
+
+export const getQuotes = async () => {
+  const response = await axios.get("/api/quotes");
+  return response;
+};
+export const getQuote = async (quoteId) => {
+  const response = await axios.get(`/api/quotes/${quoteId}`);
+  return response;
+};
